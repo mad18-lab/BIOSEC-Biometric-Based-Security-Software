19b6ee72af26343747af46920225e4cd56796a7a
Madhav <guptamadhav69@gmail.com>
2024-01-07 18:55:48 +0530

    initial commit

---
+++ app/src/main/java/com/example/biosec/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.biosec;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView notificationIcon = findViewById(R.id.notificationIcon);
+
+        notificationIcon.setOnClickListener(v -> {
+
+        });
+    }
+}

b8b87003bc22ec6263f190af09eb98ef635d4e46
Madhav <guptamadhav69@gmail.com>
2024-01-10 17:58:26 +0530

    modified changes

--- app/src/main/java/com/example/biosec/MainActivity.java
+++ app/src/main/java/com/example/biosec/MainActivity.java
@@ -18,6 +18,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -36,10 +37,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseApp.initializeApp(this);
         setContentView(R.layout.activity_main);
 
         googleAuth = findViewById(R.id.login);
-
         mAuth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance();
 

60c0c437f3b700bdf0cb0d47c054c649e5f168b3
Madhav <guptamadhav69@gmail.com>
2024-01-24 19:09:48 +0530

    modified changes

--- app/src/main/java/com/example/biosec/HomePageActivity.java
+++ app/src/main/java/com/example/biosec/HomePageActivity.java
@@ -1,12 +1,27 @@
 package com.example.biosec;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomePageActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
+    Button btnLogOut;
+    GoogleSignInClient gsc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +30,31 @@ public class HomePageActivity extends AppCompatActivity {
 
         ImageView notificationIcon = findViewById(R.id.notificationIcon);
 
-        notificationIcon.setOnClickListener(v -> {
+        mAuth = FirebaseAuth.getInstance();
+        btnLogOut = findViewById(R.id.logout);
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail().build();
+
+        gsc = GoogleSignIn.getClient(this, gso);
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signOut();
+            }
+        });
+    }
+
+    void signOut() {
+        gsc.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                finish();
+                startActivity(new Intent(HomePageActivity.this, MainActivity.class));
+            }
         });
     }
 }

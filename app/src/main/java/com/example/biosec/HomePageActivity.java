ab17cb9f8e36e2040ce3392917e58c95615cfa4e
Madhav <guptamadhav69@gmail.com>
2024-01-25 18:35:05 +0530

    modified changes

--- app/src/main/java/com/example/biosec/HomePageActivity.java
+++ app/src/main/java/com/example/biosec/HomePageActivity.java
@@ -2,6 +2,9 @@ package com.example.biosec;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -18,10 +21,15 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 public class HomePageActivity extends AppCompatActivity {
     FirebaseAuth mAuth;
-    Button btnLogOut;
+    Button btnLogOut, btnBiometric;
     GoogleSignInClient gsc;
+    BiometricPrompt biometricPrompt = null;
+    Executor executor = Executors.newSingleThreadExecutor();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +38,11 @@ public class HomePageActivity extends AppCompatActivity {
 
         ImageView notificationIcon = findViewById(R.id.notificationIcon);
 
-        mAuth = FirebaseAuth.getInstance();
-        btnLogOut = findViewById(R.id.logout);
+        mAuth = FirebaseAuth.getInstance();     //for signing users out
+        btnLogOut = findViewById(R.id.logout);  //button to sign users out
+        btnBiometric = findViewById(R.id.biometric);    //button to get biometric prompt
 
+        //functionality to sign users out
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail().build();
 
@@ -46,8 +56,69 @@ public class HomePageActivity extends AppCompatActivity {
                 signOut();
             }
         });
+
+        if(biometricPrompt==null){
+            biometricPrompt=new BiometricPrompt(this,executor,callback);
+        }
+
+        btnBiometric.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAndauth();
+            }
+        });
     }
 
+    //to check whether the device has fingerprint sensor functionality or not
+    void checkAndauth() {
+        BiometricManager biometricManager = BiometricManager.from(this);
+        switch(biometricManager.canAuthenticate()) {
+            case BiometricManager.BIOMETRIC_SUCCESS:
+                BiometricPrompt.PromptInfo promptInfo = buildPrompt();
+                biometricPrompt.authenticate(promptInfo);
+                break;
+
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                Toast.makeText(this, "Device Doesn't Support Fingerprint Sensor", Toast.LENGTH_SHORT).show();
+                break;
+
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                Toast.makeText(this, "Biometric Authentication Unsuccessful", Toast.LENGTH_SHORT).show();
+
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                Toast.makeText(this, "No Fingerprint Assigned", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //method to build biometric prompt
+    BiometricPrompt.PromptInfo buildPrompt() {
+        return new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("BioSec")
+                .build();
+    }
+
+    //method to build and authenticate biometric prompt
+    BiometricPrompt.AuthenticationCallback callback = new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                if (biometricPrompt != null)
+                    biometricPrompt.cancelAuthentication();
+                super.onAuthenticationError(errorCode, errString);
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Toast.makeText(HomePageActivity.this, "Biometric Authentication Successful", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+            }
+    };
+
+    //method to sign users out
     void signOut() {
         gsc.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override

0a7974521932059f623445aa274ac502acbe9961
Madhav <guptamadhav69@gmail.com>
2024-02-05 19:18:35 +0530

    modified changes

--- app/src/main/java/com/example/biosec/HomePageActivity.java
+++ app/src/main/java/com/example/biosec/HomePageActivity.java
@@ -6,11 +6,18 @@ import androidx.biometric.BiometricManager;
 import androidx.biometric.BiometricPrompt;
 import androidx.core.content.ContextCompat;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -36,7 +43,13 @@ public class HomePageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
 
-        ImageView notificationIcon = findViewById(R.id.notificationIcon);
+        ImageView notificationIcon = findViewById(R.id.notificationIcon);   //notification icon
+        notificationIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog();
+            }
+        });
 
         mAuth = FirebaseAuth.getInstance();     //for signing users out
         btnLogOut = findViewById(R.id.logout);  //button to sign users out
@@ -67,6 +80,20 @@ public class HomePageActivity extends AppCompatActivity {
                 checkAndauth();
             }
         });
+    }
+
+    private void showDialog() {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.bottomsheetlayout);
+
+        LinearLayout editLayout = dialog.findViewById(R.id.layoutEdit);
+
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
     }
 
     //to check whether the device has fingerprint sensor functionality or not

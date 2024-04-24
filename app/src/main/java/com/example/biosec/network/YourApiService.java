09c9fbcef511dcf1c8ce7ba31eb97507e7ed9128
Madhav Gupta <80634128+mad18-lab@users.noreply.github.com>
2024-04-25 02:26:03 +0530

    Update YourApiService.java

--- app/src/main/java/com/example/biosec/network/YourApiService.java
+++ app/src/main/java/com/example/biosec/network/YourApiService.java
@@ -1,12 +1,13 @@
 package com.example.biosec.network;
 import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
 
 public interface YourApiService {
-    @POST("register")
-    Call<Void> registerDevice(@Body RequestBody requestBody);
+    @POST("/connect")
+    Call<ResponseBody> establishConnection(@Body RequestBody requestData);
 
     @POST("unlock")
     Call<Void> unlockFolders(RequestBody requestBody);
